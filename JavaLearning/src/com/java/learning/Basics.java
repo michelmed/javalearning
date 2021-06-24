319a148b6a528fa7e376b316872e80a8c037a1a0
User <User@M6700>
2021-06-24 09:08:43 -0400

    adding function workingWithTypes()

--- JavaLearning/src/com/java/learning/Basics.java
+++ JavaLearning/src/com/java/learning/Basics.java
@@ -6,5 +6,18 @@ public class Basics {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public void workingWithTypes() {
+		
+		
+		int myNum = 5;               // Integer (whole number)
+		float myFloatNum = 5.99f;    // Floating point number
+		char myLetter = 'D';         // Character
+		boolean myBool = true;       // Boolean
+		String myText = "Hello";     // String
+		
+	}
 
 }
+
+
